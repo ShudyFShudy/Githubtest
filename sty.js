d26fc5737ad826d5d92b9947729a1791cf3a8060
Дима <Rockes350@gmail.com>
2018-05-30 14:59:56 +0300

    js up

--- sty.js
+++ sty.js
@@ -2,3 +2,15 @@ function reply_click() {
     alert("Dima Ocheretenko");
 }
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+a = prompt()
+
+b = prompt()
+
+p = getRandomInt(parseInt(a), parseInt(b))
+
+alert(p)
+
